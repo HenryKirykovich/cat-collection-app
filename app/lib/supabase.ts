1fa9894ff5ea9453dff3c78f6301a6bbb38cbd9b
HenryKirykovich <145735504+HenryKirykovich@users.noreply.github.com>
2025-05-11 18:14:39 -0700

    installed npm install @supabase/supabase-js --legacy-peer-deps

---
+++ app/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://drpxmxxffhdrmpidgkam.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRycHhteHhmZmhkcm1waWRna2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDY5MTcsImV4cCI6MjA2MjU4MjkxN30.g_-SUfA8MIqJe7V9fxTv-ikYCwP0Zwj5dXBHlUV2Ar4';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
